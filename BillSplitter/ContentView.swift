6ce082230eba8e22f306d133508c71fbe2d2f8a6
aizaz <online@aizaz.uk>
2021-10-05 20:59:44 +0400

    String converted to Int and calculations were working.

--- BillSplitter/ContentView.swift
+++ BillSplitter/ContentView.swift
@@ -30,16 +30,20 @@ struct billSplitter{
         return billforEach
         
     }
+    
+    
  
 }
 
 struct ContentView: View {
     //properrties
     var a : billSplitter = billSplitter();
-    @State var total: Double = 5000
+   
+    @State var total: Double = 999
+    @State var ppl:Double = 10
+    //-----
     @State var val = ""
-    let ppl:Double = 10
-    
+    @State var pplVal = ""
 
         //view
     var body: some View {
@@ -50,13 +54,18 @@ struct ContentView: View {
             TextField("Enter total price: ", text: $val)
                 .padding()
                 .keyboardType(.decimalPad)
+            let ax = Double(val) ?? 0
                 
+            TextField("Enter total number of people: ", text: $pplVal)
+                .padding()
+                .keyboardType(.decimalPad)
+            let bx = Double(pplVal) ?? 0
            
         //    TextField("Enter Total Price", text: total)
         
             Text("Total Bill: " + String(total)).padding()
-            Text("Bill for each: "+String(a.BillforEach(subtotal: total, numOfPeople: ppl)))
-            Text("5% Tax Incl: "+String(a.splitBy(subtotal: total, numOfPeople: ppl)))
+            Text("Bill for each: "+String(a.BillforEach(subtotal: ax, numOfPeople: bx)))
+            Text("5% Tax Incl: "+String(a.splitBy(subtotal: ax, numOfPeople: bx)))
             
                 .padding()
         }.font(.title2)
