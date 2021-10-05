1e4ef0e8128e921afd43af7d54f121357c5b5716
aizaz <online@aizaz.uk>
2021-10-05 21:39:12 +0400

    Added RoundedTextFieldsDisplay()

--- BillSplitter/ContentView.swift
+++ BillSplitter/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+
 struct taxCalculator{
     let tax: Double = 0.05
     
@@ -13,14 +14,13 @@ struct taxCalculator{
         return totalPrice
     }
 }
+
 struct billSplitter{
  
     func splitBy(subtotal:Double, numOfPeople:Double) -> (Double)
     {
         let tp : taxCalculator = taxCalculator()
         let tax:Double = subtotal*tp.tax
-       
-        
         return (tax)
     }
     
@@ -28,49 +28,43 @@ struct billSplitter{
         
         let billforEach : Double = subtotal/numOfPeople
         return billforEach
-        
     }
-    
-    
- 
+
 }
 
 struct ContentView: View {
     //properrties
     var a : billSplitter = billSplitter();
-   
-    @State var total: Double = 999
-    @State var ppl:Double = 10
     //-----
     @State var val = ""
     @State var pplVal = ""
 
         //view
     var body: some View {
-            
-        
+     
         VStack {
             
             TextField("Enter total price: ", text: $val)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
                 .keyboardType(.decimalPad)
+                
             let ax = Double(val) ?? 0
                 
             TextField("Enter total number of people: ", text: $pplVal)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
                 .keyboardType(.decimalPad)
             let bx = Double(pplVal) ?? 0
            
         //    TextField("Enter Total Price", text: total)
         
-            Text("Total Bill: " + String(total)).padding()
+            Text("Total Bill: " + String(ax)).padding()
             Text("Bill for each: "+String(a.BillforEach(subtotal: ax, numOfPeople: bx)))
             Text("5% Tax Incl: "+String(a.splitBy(subtotal: ax, numOfPeople: bx)))
-            
                 .padding()
         }.font(.title2)
     }
-    
     //methods
 }
 
